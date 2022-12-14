2df322a08ead838987c4ee4ef53a93561417bd86
M-C-AD <30936333+M-C-AD@users.noreply.github.com>
2022-12-14 16:04:59 +0000

    update

---
+++ Work/reportV4.py
@@ -0,0 +1,39 @@
+import csv
+
+def read_portfolio(filename):
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            stock = {
+                 'name'   : row[0],
+                 'shares' : int(row[1]),
+                 'price'   : float(row[2])
+            }
+            portfolio.append(stock)
+
+    return portfolio
+
+def read_prices(filename):
+    '''
+    Read a CSV file of price data into a dict mapping names to prices.
+    '''
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+    return prices
+
+portfolio = read_portfolio('../../Work/Data/portfolio.csv')
+prices    = read_prices('../../Work/Data/prices.csv')
