4dfb7352dbec00fa890a8b5363afda1779d464d3
M-C-AD <30936333+M-C-AD@users.noreply.github.com>
2022-12-21 21:47:47 +0000

    Update reportV5.py

--- Work/reportV5.py
+++ Work/reportV5.py
@@ -52,5 +52,5 @@ def make_report_data(portfolio, prices):
         
 # Read data files and create the report data        
 
-portfolio = read_portfolio('../../Work/Data/portfolio.csv')
-prices    = read_prices('../../Work/Data/prices.csv')
+portfolio = read_portfolio('Data/portfolio.csv')
+prices    = read_prices('Data/prices.csv')
